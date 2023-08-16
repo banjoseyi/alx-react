d3d2c7f4ee73798471ffa7eae656e9375b505cf1
banjoseyi <banjoseyi2@gmail.com>
2023-08-16 11:26:14 -0700

    immutable using map

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,6 +1,5 @@
 import { Map } from 'immutable';
 
-function getImmutableObject(object) {
-  return Map(object);
-}
+const getImmutableObject = (object) => Map(object);
+
 export default getImmutableObject;
